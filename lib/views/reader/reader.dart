9fd1d857ab56a4bc91ede8f2316de1d26f1a0835
Boris Sachet <boris.sachet@gmail.com>
2023-01-18 17:00:32 +0100

    Hidden android bars in reader view

--- lib/views/reader/reader.dart
+++ lib/views/reader/reader.dart
@@ -1,5 +1,6 @@
 import 'package:comic_front/views/reader/widgets/reader_gallery.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../model/file.dart';
 
@@ -13,21 +14,28 @@ class Reader extends StatefulWidget {
 }
 
 class ReaderState extends State<Reader> {
-  late bool _showAppBar;
+  late bool _showInterface;
   Axis _axis = Axis.horizontal;
   Icon _icon = const Icon(Icons.stay_current_landscape);
 
   @override
   void initState() {
-    _showAppBar = false;
+    _showInterface = false;
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: [SystemUiOverlay.bottom]); // To hide android status bar
     super.initState();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);  // to re-show bars
   }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: !_showAppBar ? null : buildAppBar(),
+      appBar: !_showInterface ? null : buildAppBar(),
       body: widget.file.pagesCount > 0 ? ReaderGallery(file: widget.file, axis: _axis, onTap: onTapAction) : Center(child: Container(),),
     );
   }
@@ -53,8 +61,13 @@ class ReaderState extends State<Reader> {
 
   void onTapAction() {
     setState(() {
-      _showAppBar = !_showAppBar;
+      _showInterface = !_showInterface;
     });
+    if (_showInterface){
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    } else {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: [SystemUiOverlay.bottom]);
+    }
   }
 
   void switchScrollDirection(){
